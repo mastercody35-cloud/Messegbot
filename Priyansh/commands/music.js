a2400119ae840c085a189bc437b794bc42b553a5
RISHI KUSHWAHA JI <baburishi641@gmail.com>
2025-05-31 10:04:41 +0530

    music.js

--- Priyansh/commands/music.js
+++ Priyansh/commands/music.js
@@ -10,7 +10,7 @@ module.exports = {
     name: "music",
     version: "1.0.3",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𓆩⃝ƦııSɦıı 𝐊͢ʋ𝛅̫̫̫ɧฬ͜ʌ̊Ӈʌ̠̠̠",
     description: "Download YouTube song from keyword search and link",
     commandCategory: "Media",
     usages: "[songName] [type]",
