6e11759b5fd27c86bee5a8b31cc56d16e7221ac1
RISHI KUSHWAHA JI <baburishi641@gmail.com>
2025-05-31 09:50:39 +0530

    adbot.js

--- Priyansh/commands/adbot.js
+++ Priyansh/commands/adbot.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "ckbot",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𓆩⃝ƦııSɦıı 𝐊͢ʋ𝛅̫̫̫ɧฬ͜ʌ̊Ӈʌ̠̠̠",
     description: "DESCRIPTION ABOUT BOT",
     commandCategory: "Media",
     usages: "",
